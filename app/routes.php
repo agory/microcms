080095b1143b72b90a5f610f13d9dc6734488182
agory <agory@TERR-4017-7210.classic.local>
2014-10-08 11:45:17 +0200

    microcms 9.1

--- app/routes.php
+++ app/routes.php
@@ -8,11 +8,29 @@ $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array('articles' => $articles));
 });
 
-// Returns detailed info about an article
-$app->get('/article/{id}', function ($id) use ($app) {
+// Detailed info about an article
+$app->match('/article/{id}', function ($id, Request $request) use ($app) {
     $article = $app['dao.article']->find($id);
+    $user = $app['security']->getToken()->getUser();
+    $commentFormView = NULL;
+    if ($app['security']->isGranted('IS_AUTHENTICATED_FULLY')) {
+        // A user is fully authenticated : he can add comments
+        $comment = new Comment();
+        $comment->setArticle($article);
+        $comment->setAuthor($user);
+        $commentForm = $app['form.factory']->create(new CommentType(), $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isValid()) {
+            $app['dao.comment']->save($comment);
+            $app['session']->getFlashBag()->add('success', 'Your comment was succesfully added.');
+        }
+        $commentFormView = $commentForm->createView();
+    }
     $comments = $app['dao.comment']->findAllByArticle($id);
-    return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
+    return $app['twig']->render('article.html.twig', array(
+        'article' => $article, 
+        'comments' => $comments,
+        'commentForm' => $commentFormView));
 });
 
 $app->get('/login', function(Request $request) use ($app) {
