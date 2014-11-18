0b296915da80eeb20297271d36f99b9e6e414fa0
agory <gory.alexandre02@gmail.coml>
2014-11-18 08:28:20 +0100

    MicroCMS IT10 Init

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInita582a6c19665e33403fc9848a5d63b8c::getLoader();
+return ComposerAutoloaderInitabde2480f8f3d1711c6ca55acfe79a2f::getLoader();
